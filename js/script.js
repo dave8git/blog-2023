64f56fd17a7d18d911fe2511c28abebf2d08818c
dave8git <dave8git@gmail.com>
2023-08-14 19:19:59 +0200

    Add links to articles

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const titleClickHandler = function(event) {
     }
     this.classList.add('active');
 
-    const articles = document.querySelectorAll('.posts article.active');
+    const articles = document.querySelectorAll(optArticleSelector);
 
     for(article of articles) {
         article.classList.remove('active');
@@ -20,6 +20,27 @@ const titleClickHandler = function(event) {
 
 }
 
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles'; // jak musi być w zmiennych/stałych... to niech jest...
+
+const generateTitleLinks = function(event) {
+    document.querySelector(optTitleListSelector).innerHTML = '';
+    const allArticles = document.querySelectorAll(optArticleSelector);
+    let html = '';
+    for(article of allArticles) {
+        const articleId = article.getAttribute('id');
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        html += '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    }
+
+    document.querySelector('.titles').innerHTML = html;
+
+    console.log(html);
+}
+
+
+generateTitleLinks();
 
 const links = document.querySelectorAll('.titles a');
 
